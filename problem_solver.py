9f85414b2e9e8ed8ea8a9b148b0430d3fcb36621
PeterdeGuzman <pdeguz01@Peters-MacBook-Pro-5.local>
2025-11-19 17:55:32 -0500

    adding progress

--- problem_solver.py
+++ problem_solver.py
@@ -1,12 +1,26 @@
 import argparse
+from google import genai
+import pandas as pd
 
 def main():
+    #Define parser and args
     parser = argparse.ArgumentParser(description="A simple command-line tool to help solve math problems.")
-
-    parser.add_argument("prob", help="Please provide a math problem for Gemini to solve.")
-
+    parser.add_argument("prob_num", help="Please provide a number associated with a math problem for Gemini to solve.")
     args = parser.parse_args()
-    print(f"{args.prob}")
+    print(f"{args.prob_num}")
+    
+    #Upload list of problems
+    problem_list = pd.read_csv("math_data_summarized.csv")
 
+    question_text = problem_list.iloc[0:args.prob_num]
+    
+    
+
+    #Call Gemini
+    client = genai.Client()
+    response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=f"Provide an answer to {args.prob}"
+)
+    print(response.text)
 if __name__ == "__main__":
     main()
